6bd657f19a7ac4d293b7d9df9933f532ad09eb9c
Jérémie Astori <jeremie@astori.fr>
2014-03-14 00:21:09 -0400

    Making all the commands run quietly

--- cryptic.sh
+++ cryptic.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # Shrink the root logical volume after checking (e2fsck) and shrinking (resize2fs) the root fs
-sudo lvresize --resizefs --size 20G /dev/ubuntu-vg/root
+sudo lvresize --resizefs --size 20G /dev/ubuntu-vg/root > /dev/null 2>&1
 
 # Create a logical volume the size of all remaining space in the volume group
-sudo lvcreate --extents 100%FREE --name home ubuntu-vg
+sudo lvcreate --extents 100%FREE --name home ubuntu-vg > /dev/null
 
 # Create an ext4 filesystem for home (taking the whole space in the logical volume)
-sudo mkfs.ext4 /dev/ubuntu-vg/home
+sudo mkfs.ext4 -q /dev/ubuntu-vg/home
 
 # Check the 2 filesystems after operations
-sudo e2fsck -f /dev/ubuntu-vg/root
-sudo e2fsck -f /dev/ubuntu-vg/home
+sudo e2fsck -f /dev/ubuntu-vg/root > /dev/null 2>&1
+sudo e2fsck -f /dev/ubuntu-vg/home > /dev/null 2>&1
 
 # Retrieve the home content from the original root partition
 sudo mkdir -p /mnt/ubuntu-{root,home}
@@ -21,7 +21,7 @@ sudo rsync -aXS /mnt/ubuntu-root/home/. /mnt/ubuntu-home/.
 sudo rm -rf /mnt/ubuntu-root/home/*
 
 # Update fstab to mount the home volume at startup
-echo "/dev/mapper/ubuntu--vg-home  /home  ext4  defaults  0  2" | sudo tee --append /mnt/ubuntu-root/etc/fstab
+echo "/dev/mapper/ubuntu--vg-home  /home  ext4  defaults  0  2" | sudo tee --append /mnt/ubuntu-root/etc/fstab > /dev/null
 
 # Success message
 printf "Success! You can now turn off your computer.\n"
