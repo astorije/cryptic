b9b903a869614b73a06d15b9ad6f06b55df55384
Jérémie Astori <jeremie@astori.fr>
2014-03-14 00:01:28 -0400

    Moving the old home to the new /home

--- cryptic.sh
+++ cryptic.sh
@@ -8,3 +8,10 @@ sudo lvcreate --extents 100%FREE --name home ubuntu-vg
 
 # Create an ext4 filesystem for home (taking the whole space in the logical volume)
 sudo mkfs.ext4 /dev/ubuntu-vg/home
+
+# Retrieve the home content from the original root partition
+sudo mkdir -p /mnt/ubuntu-{root,home}
+sudo mount /dev/ubuntu-vg/root /mnt/ubuntu-root
+sudo mount /dev/ubuntu-vg/home /mnt/ubuntu-home
+sudo rsync -aXS /mnt/ubuntu-root/home/. /mnt/ubuntu-home/.
+sudo rm -rf /mnt/ubuntu-root/home/*
